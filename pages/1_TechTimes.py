ee60ad6afa28d39354f4befe71970b9eaedce8d5
ericda2005 <ericda2005@gmail.com>
2026-07-29 15:33:26 +0800

    Update and rename 1_technews.py to 1_TechTimes.py

--- pages/1_TechTimes.py
+++ pages/1_TechTimes.py
@@ -422,7 +422,7 @@ if st.session_state.final_summary_df is not None and not st.session_state.final_
 
         categories_str = "_".join(selected_categories) if selected_categories else "all"
         
-        file_name = f"{current_date}_techtimes_{categories_str}.csv"
+        file_name = f"{current_date}_TechTimes_{categories_str}.csv"
         
         # 轉成 CSV 格式 (utf-8-sig 確保 Excel 開啟不亂碼)
         csv_summary = st.session_state.final_summary_df.to_csv(index=False, encoding="utf-8-sig", doublequote=True).encode("utf-8-sig")
